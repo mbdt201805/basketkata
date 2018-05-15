cdf5126f4a67ec9f841bef03a9400795cc1b8f15
Matthew Butt <matthew@codurance.com>
2018-05-15 08:42:04 +0100

    Implement empty basket -> £0.00
    - The first scenario in the instructions (1 bread, 1 butter & 1 milk) represents a substantial chunk of functionality, so I'm going to start with the simplest possible case.
    - I've created a specific `Gbp` type to represent the money, rather than a generic `Money` type. I think we may have to implement addition, and it's easier not to have to think about currency conversion!
    - I'm not going to move `Gbp` and `Basket` to new files quite yet. Let's see how the code evolves.
    - I'm not going to convert `Total` to an expression body, as I anticipate quite a bit of code in here, at least to begin with.
    - Implementing `ToString` on `Gbp` gives me useful feedback when my tests fail.

--- BasketKata.Tests/BasketShould.cs
+++ BasketKata.Tests/BasketShould.cs
@@ -1,9 +1,42 @@
 ﻿using NUnit.Framework;
+using Shouldly;
 
 namespace BasketKata.Tests
 {
     [TestFixture]
     public class BasketShould
     {
+        [Test]
+        public void HaveTotalZero_WhenEmpty()
+        {
+            var basket = new Basket();
+
+            var total = basket.Total;
+
+            total.ShouldBe(new Gbp(0m));
+        }
+    }
+
+    public struct Gbp
+    {
+        private readonly decimal _amount;
+
+        public Gbp(decimal amount)
+        {
+            _amount = amount;
+        }
+
+        public override string ToString() => $"£{_amount:0.00}";
+    }
+
+    public class Basket
+    {
+        public Gbp Total
+        {
+            get
+            {
+                return new Gbp(0m);
+            }
+        }
     }
 }
