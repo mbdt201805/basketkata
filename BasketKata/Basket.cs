8aaa173845c7dab7aba8da9d189d49b16bffa268
Matthew Butt <matthew@codurance.com>
2018-05-15 11:27:32 +0100

    Refactor: pull pricingRules into a field

--- BasketKata/Basket.cs
+++ BasketKata/Basket.cs
@@ -7,11 +7,19 @@ namespace BasketKata
     public class Basket
     {
         private readonly IList<Item> _items = new List<Item>();
-        private readonly BreadAndButterRule _breadAndButterRule = new BreadAndButterRule();
-        private readonly MultiMilkRule _multiMilkRule = new MultiMilkRule();
-        private readonly ButterRule _butterRule = new ButterRule();
-        private readonly MilkRule _milkRule = new MilkRule();
-        private readonly BreadRule _breadRule = new BreadRule();
+        private readonly IPricingRule[] _pricingRules;
+
+        public Basket()
+        {
+            _pricingRules = new IPricingRule[]
+            {
+                new BreadAndButterRule(),
+                new MultiMilkRule(),
+                new ButterRule(),
+                new MilkRule(),
+                new BreadRule()
+            };
+        }
 
         public void Add(Item item) => _items.Add(item);
 
@@ -19,9 +27,7 @@ namespace BasketKata
 
         private RunningTotal Calculate(RunningTotal runningTotal)
         {
-            var rules = new IPricingRule[] {_breadAndButterRule, _multiMilkRule, _butterRule, _milkRule, _breadRule};
-
-            return rules.Aggregate(runningTotal, (rt, rule) => rule.Apply(rt));
+            return _pricingRules.Aggregate(runningTotal, (rt, rule) => rule.Apply(rt));
         }
 
         private int Count(Item item) => _items.Count(i => i == item);
