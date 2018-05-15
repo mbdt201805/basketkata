9c458136b277ea5026cce18b89da0f6741817d11
Matthew Butt <matthew@codurance.com>
2018-05-15 10:10:43 +0100

    Refactor: introduct running total

--- BasketKata/Basket.cs
+++ BasketKata/Basket.cs
@@ -4,6 +4,22 @@ using static BasketKata.Item;
 
 namespace BasketKata
 {
+    public class RunningTotal
+    {
+        public RunningTotal(int countButter, int countBread, int countMilk, Gbp total)
+        {
+            CountButter = countButter;
+            CountBread = countBread;
+            CountMilk = countMilk;
+            Total = total;
+        }
+
+        public int CountButter { get; set; }
+        public int CountBread { get; set; }
+        public int CountMilk { get; set; }
+        public Gbp Total { get; set; }
+    }
+
     public class Basket
     {
         private readonly IList<Item> _items = new List<Item>();
@@ -15,26 +31,32 @@ namespace BasketKata
             get
             {
                 var countButter = Count(Butter);
-                var countMilk = Count(Milk);
                 var countBread = Count(Bread);
+                var countMilk = Count(Milk);
                 var total = new Gbp(0.00m);
-                while (countButter >= 2 && countBread >= 1)
-                {
-                    total += new Gbp(2.10m);
-                    countButter -= 2;
-                    countBread -= 1;
-                }
-
-                while (countMilk >= 4)
-                {
-                    total += new Gbp(3.45m);
-                    countMilk -= 4;
-                }
-                total += countButter * new Gbp(0.80m);
-                total += countMilk * new Gbp(1.15m);
-                total += countBread * new Gbp(1.00m);
-                return total;
+                return Calculate(new RunningTotal(countButter, countBread, countMilk, total));
             }
+        }
+
+        private static Gbp Calculate(RunningTotal runningTotal)
+        {
+            while (runningTotal.CountButter >= 2 && runningTotal.CountBread >= 1)
+            {
+                runningTotal.Total += new Gbp(2.10m);
+                runningTotal.CountButter -= 2;
+                runningTotal.CountBread -= 1;
+            }
+
+            while (runningTotal.CountMilk >= 4)
+            {
+                runningTotal.Total += new Gbp(3.45m);
+                runningTotal.CountMilk -= 4;
+            }
+
+            runningTotal.Total += runningTotal.CountButter * new Gbp(0.80m);
+            runningTotal.Total += runningTotal.CountMilk * new Gbp(1.15m);
+            runningTotal.Total += runningTotal.CountBread * new Gbp(1.00m);
+            return runningTotal.Total;
         }
 
         private int Count(Item item) => _items.Count(i => i == item);
