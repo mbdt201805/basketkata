0963bb7b43ae629341de452fcce3b9bc19becaa1
Matthew Butt <matthew@codurance.com>
2018-05-15 09:53:28 +0100

    Refactor: inline pricing methods. We need to change the way we do this.

--- BasketKata/Basket.cs
+++ BasketKata/Basket.cs
@@ -21,18 +21,12 @@ namespace BasketKata
                     return 2 * new Gbp(0.80m) + 1 * new Gbp(0.50m) + 1 * new Gbp(1.00m);
                 if (Count(Butter) == 4 && Count(Bread) == 4)
                     return 4 * new Gbp(0.80m) + 2 * new Gbp(0.50m) + 2 * new Gbp(1.00m);
-                total += ButterPrice();
-                total += MilkPrice();
-                total += BreadPrice();
+                total += Count(Butter) * new Gbp(0.80m);
+                total += Count(Milk) * new Gbp(1.15m);
+                total += Count(Bread) * new Gbp(1.00m);
                 return total;
             }
         }
-
-        private Gbp ButterPrice() => Count(Butter) * new Gbp(0.80m);
-
-        private Gbp MilkPrice() => Count(Milk) * new Gbp(1.15m);
-
-        private Gbp BreadPrice() => Count(Bread) * new Gbp(1.00m);
 
         private int Count(Item item) => _items.Count(i => i == item);
     }
