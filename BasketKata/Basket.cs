8fd449bba163012f4e3091f0ae8cb7cd6a1592f6
Matthew Butt <matthew@codurance.com>
2018-05-15 09:59:22 +0100

    Refactor: replace `if`s with `while`

--- BasketKata/Basket.cs
+++ BasketKata/Basket.cs
@@ -18,26 +18,12 @@ namespace BasketKata
                 var countMilk = Count(Milk);
                 var countBread = Count(Bread);
                 var total = new Gbp(0.00m);
-                if (countButter == 2 && countBread == 1)
+                while (countButter >= 2 && countBread >= 1)
                 {
                     total += new Gbp(2.10m);
                     countButter -= 2;
                     countBread -= 1;
                 }
-
-                if (countButter == 2 && countBread == 2)
-                {
-                    total += new Gbp(2.10m);
-                    countButter -= 2;
-                    countBread -= 1;
-                }
-                if (countButter == 4 && countBread == 4)
-                {
-                    total += new Gbp(4.20m);
-                    countButter -= 4;
-                    countBread -= 2;
-                }
-
                 total += countButter * new Gbp(0.80m);
                 total += countMilk * new Gbp(1.15m);
                 total += countBread * new Gbp(1.00m);
