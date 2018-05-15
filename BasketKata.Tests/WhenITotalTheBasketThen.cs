c388d1482bbe5d7548cccfe1adae30e4b069144b
Matthew Butt <matthew@codurance.com>
2018-05-15 17:41:42 +0100

    Remove tests used to drive the functionality
    (I've left the null case, as I find it helpful to define this behaviour)

--- BasketKata.Tests/WhenITotalTheBasketThen.cs
+++ BasketKata.Tests/WhenITotalTheBasketThen.cs
@@ -18,55 +18,6 @@ namespace BasketKata.Tests
         }
 
         [Test]
-        public void TheTotalShouldBe80Pence_GivenTheBasketHas1Butter()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(0.80m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe1Pound60_GivenTheBasketHas2Butter()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-            basket.Add(Butter);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(1.60m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe1Pound95_GivenTheBasketHas1ButterAnd1Milk()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-            basket.Add(Milk);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(1.95m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe3Pounds90_GivenTheBasketHas2ButterAnd2Milk()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-            basket.Add(Milk);
-            basket.Add(Butter);
-            basket.Add(Milk);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(3.90m));
-        }
-
-        [Test]
         public void TheTotalShouldBe2Pounds95_GivenTheBasketHas1Butter1MilkAnd1Bread()
         {
             var basket = new Basket();
@@ -77,19 +28,6 @@ namespace BasketKata.Tests
             var total = basket.Total;
 
             total.ShouldBe(new Gbp(2.95m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe2Pounds10_GivenTheBasketHas2ButterAnd1Bread()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-            basket.Add(Butter);
-            basket.Add(Bread);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(2.10m));
         }
 
         [Test]
@@ -104,38 +42,6 @@ namespace BasketKata.Tests
             var total = basket.Total;
 
             total.ShouldBe(new Gbp(3.10m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe6Pounds20_GivenTheBasketHas4ButterAnd4Bread()
-        {
-            var basket = new Basket();
-            basket.Add(Butter);
-            basket.Add(Butter);
-            basket.Add(Bread);
-            basket.Add(Bread);
-            basket.Add(Butter);
-            basket.Add(Butter);
-            basket.Add(Bread);
-            basket.Add(Bread);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(6.20m));
-        }
-
-        [Test]
-        public void TheTotalShouldBe3Pounds45_GivenTheBasketHas4Milk()
-        {
-            var basket = new Basket();
-            basket.Add(Milk);
-            basket.Add(Milk);
-            basket.Add(Milk);
-            basket.Add(Milk);
-
-            var total = basket.Total;
-
-            total.ShouldBe(new Gbp(3.45m));
         }
 
         [Test]
