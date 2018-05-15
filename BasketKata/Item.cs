5583d782ccc85c8e3aa9dbbaeed1c7b3ef3b443f
Matthew Butt <matthew@codurance.com>
2018-05-15 14:14:39 +0100

    Refactor: convert Item to abstract class with subclasses

--- BasketKata/Item.cs
+++ BasketKata/Item.cs
@@ -1,9 +1,13 @@
 ﻿namespace BasketKata
 {
-    public class Item
+    public abstract class Item
     {
-        public static Item Butter { get; } = new Item();
-        public static Item Milk { get; } = new Item();
-        public static Item Bread { get; } = new Item();
+        public static Item Butter { get; } = new ButterItem();
+        public static Item Milk { get; } = new MilkItem();
+        public static Item Bread { get; } = new BreadItem();
+
+        private class ButterItem : Item { }
+        private class MilkItem : Item { }
+        private class BreadItem : Item { }
     }
 }
