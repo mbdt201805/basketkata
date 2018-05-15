d7f6acf92354e4c196afe347b7d71af25b1a1f69
Matthew Butt <matthew@codurance.com>
2018-05-15 11:16:45 +0100

    Refactor: extract rules as methods

--- BasketKata/Basket.cs
+++ BasketKata/Basket.cs
@@ -14,30 +14,65 @@ namespace BasketKata
 
         private static RunningTotal Calculate(RunningTotal runningTotal)
         {
+            runningTotal = ApplyBreadAndButterRule(runningTotal);
+
+            runningTotal = ApplyMultiMilkRule(runningTotal);
+
+            runningTotal = ApplyButterRule(runningTotal);
+
+            runningTotal = ApplyMilkRule(runningTotal);
+
+            runningTotal = ApplyBreadRule(runningTotal);
+
+            return runningTotal;
+        }
+
+        private static RunningTotal ApplyBreadAndButterRule(RunningTotal runningTotal)
+        {
             while (runningTotal.CountButter >= 2 && runningTotal.CountBread >= 1)
             {
                 runningTotal = new RunningTotal(runningTotal.CountButter - 2, runningTotal.CountBread - 1,
                     runningTotal.CountMilk, runningTotal.Total + new Gbp(2.10m));
             }
 
+            return runningTotal;
+        }
+
+        private static RunningTotal ApplyMultiMilkRule(RunningTotal runningTotal)
+        {
             while (runningTotal.CountMilk >= 4)
             {
                 runningTotal = new RunningTotal(runningTotal.CountButter, runningTotal.CountBread,
-                    runningTotal.CountMilk -4, runningTotal.Total+ new Gbp(3.45m));
+                    runningTotal.CountMilk - 4, runningTotal.Total + new Gbp(3.45m));
             }
 
+            return runningTotal;
+        }
+
+        private static RunningTotal ApplyButterRule(RunningTotal runningTotal)
+        {
             while (runningTotal.CountButter >= 1)
             {
                 runningTotal = new RunningTotal(runningTotal.CountButter - 1, runningTotal.CountBread,
                     runningTotal.CountMilk, runningTotal.Total + new Gbp(0.80m));
             }
 
+            return runningTotal;
+        }
+
+        private static RunningTotal ApplyMilkRule(RunningTotal runningTotal)
+        {
             while (runningTotal.CountMilk >= 1)
             {
                 runningTotal = new RunningTotal(runningTotal.CountButter, runningTotal.CountBread,
                     runningTotal.CountMilk - 1, runningTotal.Total + new Gbp(1.15m));
             }
 
+            return runningTotal;
+        }
+
+        private static RunningTotal ApplyBreadRule(RunningTotal runningTotal)
+        {
             while (runningTotal.CountBread >= 1)
             {
                 runningTotal = new RunningTotal(runningTotal.CountButter, runningTotal.CountBread - 1,
